053affe14baf655fb925e719dc25a8a5d7d66aa4
arturhbs <arturhenrique71@gmail.com>
2019-05-09 12:07:54 -0300

    Add split data with train and test and making a new predict with those new variables

--- iris.py
+++ iris.py
@@ -31,3 +31,23 @@ species =  knn.predict([[5.1,3.5,1.4,0.2]])
 
 print "Previsão: Está relacionado com a planta ", iris.target_names[species]
 
+# Separar dados em dois grupos(um para teste e outro para treinamento)
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
+
+# Treinar com os novos valores para teste
+knn.fit(x_train,y_train)
+previsao = knn.predict(x_test)
+print "Previsão a partir do exemplos teste : ", previsao
+print "O resultado final deveria ser :       ", y_test
+# Verificar a acuracia dos resultados
+
+from sklearn import metrics
+acertos = metrics.accuracy_score(y_test, previsao)
+print acertos
+
+
+
+
