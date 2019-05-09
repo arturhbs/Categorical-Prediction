ac38fdc737203ab344edbbab49e05dee4b1c78ae
arturhbs <arturhenrique71@gmail.com>
2019-05-09 11:31:54 -0300

    Add KnnClassifier and make one prediction to test output

--- iris.py
+++ iris.py
@@ -20,3 +20,14 @@ print (iris.target.shape)
 
 # Usando KNN para treinamento dos dados
 
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(x,y)
+
+#fazer previsão
+
+species =  knn.predict([[5.1,3.5,1.4,0.2]])
+
+print "Previsão: Está relacionado com a planta ", iris.target_names[species]
+
